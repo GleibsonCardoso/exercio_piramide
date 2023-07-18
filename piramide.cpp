0a153188feeb34b5d8da858925e8486e8b4a3595
GleibsonCardoso <59875267+GleibsonCardoso@users.noreply.github.com>
2023-07-18 00:28:30 -0300

    Create piramide.cpp

---
+++ piramide.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+
+int n;
+cout << "Digite o numero de linhas";
+cin >> n;
+
+for (int i=1; i<=n; i++){
+    for (int j=n; j > 0; j--){
+      if (i >= j){
+        cout << j << ' ';
+      }else{
+        cout << " ";
+      }
+    }
+    cout << endl;
+  }
+}
